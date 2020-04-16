ac4ca94d3ec572976e222784a62939fdb1e6ac9b
Unknown <arthur.delamare1@viacesi.fr>
2020-04-16 22:14:24 +0200

    feat(generate-package): check if files already exist

--- leon-cli.js
+++ leon-cli.js
@@ -1,5 +1,7 @@
-const { program } = require('commander');
+const fs = require('fs');
 const path = require('path');
+const program = require('commander');
+
 
 program.version('0.0.1');
 
@@ -8,8 +10,25 @@ program
     .description('generate a package and its structure')
     .option('-f, --flat', 'determine if a folder has to created', false)
     .action(function(name, options) {
-        packagePath = options.flat ? __dirname : path.join(__dirname, name)
-        console.log(packagePath)
-    })
+        const packagePath = options.flat ? __dirname : path.join(__dirname, name);
+
+        const files = [
+            path.join('config', 'config.json'),
+            path.join('config', 'config.sample.json'),
+            path.join('data', 'answer', 'en'),
+            path.join('data', 'db', '.gitkeep'),
+            path.join('data', 'expression', 'en'),
+            path.join('test', '.gitkeep'),
+            'README.md',
+            'version.txt'
+        ];
+        for(const element of files) {
+            if (fs.existsSync(path.join(packagePath, element))) {
+                throw new Error(`File at ${path.join(packagePath, element)} already exists.`);
+            } 
+        }
+
+
+    });
 
 program.parse(process.argv);
