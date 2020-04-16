f24413ac59451d4b69cc8e98c962d1764a82c0b2
Unknown <arthur.delamare1@viacesi.fr>
2020-04-16 23:36:45 +0200

    feat(generate-package): change template directory, add languages options

--- leon-cli.js
+++ leon-cli.js
@@ -5,10 +5,15 @@ const program = require('commander');
 
 program.version('0.0.1');
 
+function addLanguage(language, languageList) {
+    return languageList.concat([language])
+}
+
 program
     .command('generate-package <name>')
     .description('generate a package and its structure')
     .option('-f, --flat', 'determine if a folder has to created', false)
+    .option('-s, --spoken-languages <language>', 'list of spoken languages available in the package', addLanguage, [])
     .action(function(name, options) {
         const packagePath = options.flat ? process.cwd() : path.join(process.cwd(), name);
 
@@ -22,6 +27,12 @@ program
             'README.md',
             'version.txt'
         ];
+
+        for (const language of options.spokenLanguages) {
+            files.push(path.join('data', 'answer', language));
+            files.push(path.join('data', 'expression', language));
+        }
+
         for(const element of files) {
             if (fs.existsSync(path.join(packagePath, element))) {
                 throw new Error(`File at ${path.join(packagePath, element)} already exists.`);
@@ -29,7 +40,7 @@ program
         }
 
         ncp.limit = 16;
-        ncp(path.join(__dirname, 'template'), packagePath, function(err) {
+        ncp(path.join(__dirname, 'template', 'package'), packagePath, function(err) {
             if (err) {
                 return console.error(err);
               }
